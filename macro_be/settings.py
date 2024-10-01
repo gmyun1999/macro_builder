85d1f7f21e7f946a1e647720a6079df9fd750b45
gmyun1999 <dbsrbals26@gmail.com>
2024-10-01 19:23:44 +0900

    refactor: env location

--- macro_be/settings.py
+++ macro_be/settings.py
@@ -18,20 +18,22 @@ from pathlib import Path
 import environ
 import pymysql
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(env_file=BASE_DIR / ".env")
+
 
 ENV = env("ENV", default="localhost")
 print(ENV)
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env("SECRET_KEY", default="")
+SECRET_KEY = env("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
