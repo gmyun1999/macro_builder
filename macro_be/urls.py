9b0792ebed1862589898a155261401ce328d60c8
gmyun1999 <dbsrbals26@gmail.com>
2024-11-19 11:20:45 +0900

    chore

--- macro_be/urls.py
+++ macro_be/urls.py
@@ -23,12 +23,6 @@ urlpatterns = [
     re_path(r"^$", view=HealthChecker.as_view(), name="HealthChecker"),
     re_path(r"^", include(user_urls)),
     re_path(r"^", include(macro_urls)),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
