62005a2c4fb2d054bb3d0263bd568d0bf038b2e6
gmyun1999 <dbsrbals26@gmail.com>
2024-11-19 09:47:26 +0900

    Feat: swaggerc

--- macro_be/settings.py
+++ macro_be/settings.py
@@ -100,6 +100,7 @@ INSTALLED_APPS = [
     "macro_sheet",
     "django_extensions",
     "django_cte",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
@@ -209,7 +210,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- macro_be/urls.py
+++ macro_be/urls.py
@@ -1,11 +1,34 @@
-from django.urls import include, re_path
+from django.conf.urls.static import static
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+from macro_be import settings
 from macro_be.views import HealthChecker
 from macro_sheet.interface import urls as macro_urls
 from user.interface import urls as user_urls
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="macro-be API",
+        default_version="v1",
+        description="API documentation for the macro-be project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     re_path(r"^$", view=HealthChecker.as_view(), name="HealthChecker"),
     re_path(r"^", include(user_urls)),
     re_path(r"^", include(macro_urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
