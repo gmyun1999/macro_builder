26e0e2a9862028e58116cded793273b4039e3e56
gmyun1999 <dbsrbals26@gmail.com>
2024-11-19 12:08:58 +0900

    feat: swagger

--- macro_be/settings.py
+++ macro_be/settings.py
@@ -211,10 +211,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-if ENV == "localhost":
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-else:
-    STATIC_ROOT = env("STATIC_ROOT", default="")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
--- macro_be/views.py
+++ macro_be/views.py
@@ -1,4 +1,7 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.renderers import SwaggerUIRenderer
 from rest_framework import status
 from rest_framework.views import APIView
 
@@ -11,3 +14,28 @@ class HealthChecker(APIView):
             data={"status": "success", "ENV": ENV},
             status=status.HTTP_200_OK,
         )
+
+
+def swagger_html_view(request):
+    schema_generator = OpenAPISchemaGenerator(
+        info=openapi.Info(
+            title="Your API",  # API 제목
+            default_version="1.0.0",  # 버전을 명확히 설정
+            description="API 문서",  # 설명
+            terms_of_service="https://www.google.com/policies/terms/",
+            contact=openapi.Contact(email="contact@example.com"),
+            license=openapi.License(name="BSD License"),
+        )
+    )
+
+    # 스키마 생성 시 request=None으로 설정하여 호환성 유지
+    schema = schema_generator.get_schema(request=None, public=True)
+    renderer = SwaggerUIRenderer()
+    renderer_context = {
+        "request": request,
+        "url": request.build_absolute_uri(),
+    }
+
+    # HTML 콘텐츠 생성 및 반환
+    html_content = renderer.render(schema, renderer_context=renderer_context)
+    return HttpResponse(html_content, content_type="text/html")
