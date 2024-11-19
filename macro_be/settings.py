ff044e919c7aa3228ae381b4d6469c84ce739544
gmyun1999 <dbsrbals26@gmail.com>
2024-11-19 11:09:41 +0900

    refactor: settting

--- macro_be/settings.py
+++ macro_be/settings.py
@@ -211,7 +211,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+if ENV == "localhost":
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+else:
+    STATIC_ROOT = env("STATIC_ROOT", default="")
 
 
 # Default primary key field type
